2786610d01b7db52a94a0a0e32c699a828fd40a1
mgu9001 <manuel.guevara@fifa.org>
2025-01-16 20:32:27 -0300

    Add project files.

---
+++ Problems.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithmsInCsharp
+{
+    public class Problems
+    {
+        public static int[] BruteForceTwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1 ; j < nums.Length; j++)
+                {
+                    if((nums[i] + nums[j]) == target)
+                    {
+                        return [i, j];
+                    }
+                }
+            }
+
+            return [];
+
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var pairDic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (pairDic.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { pairDic[target - nums[i]], i };
+                }
+                pairDic.TryAdd(nums[i], i);
+            }
+            return new int[] { };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using algorithmsInCsharp;
+
+Console.WriteLine("Hello, World!");
+
+Problems.TwoSum(new int[] { 3, 2, 4 }, 6);
+
+
