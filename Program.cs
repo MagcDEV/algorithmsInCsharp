889316792d97ed5d2ef2504ed560495fcefc4dad
mgu9001 <manuel.guevara@fifa.org>
2025-01-19 14:32:07 -0300

    Remove test code for TopKFrequent method
    Removed initialization of nums and target variables, the call to TopKFrequent method, and the foreach loop that prints the result.

--- Program.cs
+++ Program.cs
@@ -3,13 +3,3 @@
 using algorithmsInCsharp;
 
 Console.WriteLine("Hello, World!");
-
-var nums = new int[] {1,1,1, 3,3, 4 };
-var target = 6;
-
-var result = Problems.TopKFrequent(nums, 2);
-
-foreach (var item in result)
-{
-    Console.WriteLine(item);
-};
